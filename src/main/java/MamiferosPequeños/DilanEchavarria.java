bc395271f52db9ce71d0c27ca3d0c23f5ffcbb04
Dilan <dilanechavarria68@gmail.com>
2025-11-19 15:29:44 -0500

    feat se le modifico el nombre de los sout

--- src/main/java/MamiferosPequeños/DilanEchavarria.java
+++ src/main/java/MamiferosPequeños/DilanEchavarria.java
@@ -12,21 +12,21 @@ public class DilanEchavarria {
         while (!desicion.equals("SALIR")) {
             HashMap<String, Object> mamiferoPequenho = new HashMap<>();
 
-            System.out.print("Ingrese el nombre: ");
+            System.out.print("Ingrese el nombre del mamífero pequeño: ");
             mamiferoPequenho.put("nombre", pedirDato.next());
 
-            System.out.print("Ingrese el Id: ");
+            System.out.print("Ingrese el Id del mamífero pequeño: ");
             mamiferoPequenho.put("Id", pedirDato.nextInt());
 
-            System.out.print("Ingrese la edad: ");
+            System.out.print("Ingrese la edad del mamífero pequeño: ");
             mamiferoPequenho.put("edad" , pedirDato.nextInt());
 
             pedirDato.nextLine();
 
-            System.out.print("Ingrese la fecha de entrada: ");
+            System.out.print("Ingrese la fecha de entrada del mamífero pequeño: ");
             mamiferoPequenho.put("fechaEntrada" , pedirDato.nextLine());
 
-            System.out.print("Ingrese los alimentos: ");
+            System.out.print("Ingrese los alimentos del mamífero pequeño: ");
             mamiferoPequenho.put("alimentos" , pedirDato.nextLine());
 
             mamiferosPequenhos.add(mamiferoPequenho);
